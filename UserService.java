86c7e20e4e8e03fd5414e6c55b0bdd98a3150d41
Pranab Prabhanjan Sahoo <pranab.sahoo135@gmail.com>
2026-01-17 20:23:43 +0530

    Add files via upload

---
+++ UserService.java
@@ -0,0 +1,29 @@
+package com.example.demo.service;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public String registerUser(User user) {
+        if (userRepository.findByEmail(user.getEmail()) != null) {
+            return "Error: Email already exists!";
+        }
+        userRepository.save(user);
+        return "Success: User registered!";
+    }
+
+    public User authenticate(String email, String password) {
+        User user = userRepository.findByEmail(email);
+        if (user != null && user.getPassword().equals(password)) {
+            return user;
+        }
+        return null;
+    }
+}
